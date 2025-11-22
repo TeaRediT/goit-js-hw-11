153154e2752388ce2a4221b1a5c8fdeab969cac7
TeaRediT <vlad.bobro.02@gmail.com>
2025-11-22 19:26:03 +0200

    some work

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  clearGallery,
+  createGallery,
+  errorToast,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
+
+export const refs = {
+  formEl: document.querySelector('.form'),
+  inputEl: document.querySelector("input[name='search-text']"),
+  listEl: document.querySelector('.gallery'),
+};
+
+refs.formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  clearGallery();
+  showLoader();
+  const input = refs.inputEl.value.trim();
+
+  getImagesByQuery(input)
+    .then(images => {
+      hideLoader();
+      //images not found
+      if (images.length === 0) {
+        errorToast(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+        return;
+      }
+      //images found
+      createGallery(images);
+    })
+    .catch(() => {
+      errorToast('ERROR');
+    });
+  refs.formEl.reset();
+});
